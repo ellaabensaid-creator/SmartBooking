75549ac96158e9848893e239f60d1642d5e28bc6
Ella <ellaa.bensaid@sesame.com.tn>
2026-09-02 05:20:50 +0100

    Add SSL configuration to database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: env.dbUser,
   password: env.dbPassword,
   database: env.dbName,
+  ssl: {},
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
